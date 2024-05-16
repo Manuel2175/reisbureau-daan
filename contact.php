14b22600e73e455ad43d566876559117a891f5db
M-Raaijmakers1021 <1206709@student.roc-nijmegen.nl>
2024-05-16 13:23:48 +0200

    verwerkt van header

---
+++ contact.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/contact.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>opdracht</title>
+</head>
+
+<body>
+    <?php
+    include ('header.php');
+    ?>
+    <main>
+
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
